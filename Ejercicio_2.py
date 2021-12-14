2346867c311a982ba35e832c0785d22bf5876e63
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-14 11:23:44 +0100

    Establecemos la posicion de las piezas de ajedrez

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -14,3 +14,18 @@ z = random.randint(0,2)
 a = random.randint(0,2)
 b = random.randint(0,2)
 c = random.randint(0,2)
+
+while x == a:
+    a = random.randint(0,2)
+while y == b:
+    b = random.randint(0,2)
+while z == c:
+    c = random.randint(0,2)
+
+# Establecemos la posicion de las piezas
+tableroajedrez[x][0] = chr(0x2656)
+tableroajedrez[y][1] = chr(0x2656)
+tableroajedrez[z][2] = chr(0x2656)
+tableroajedrez[a][0] = chr(0x265C)
+tableroajedrez[b][1] = chr(0x265C)
+tableroajedrez[c][2] = chr(0x265C)
