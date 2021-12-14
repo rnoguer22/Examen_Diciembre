6d6dbffa06b34329c57b97c93b8777db46923254
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-14 11:28:39 +0100

    definimos la funcion encerrada

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -7,6 +7,20 @@ def printeartablero(tableroajedrez):
         contador_indice += 1
     print("\n")   # Salto de linea para los futuros tableros
 
+def encerrada(fila, columna):
+    if fila == 0 and tableroajedrez[fila+1][columna] != ' ':
+        error = True   # Esta condicion se cumple si abajo hay una ficha
+    elif fila == 1:
+        if tableroajedrez[fila+1][columna] != ' ' and tableroajedrez[fila-1][columna] != ' ':
+            error = True   # Esta condicion se cumple si arriba y abajo hay fichas
+        else:
+            error = False
+    elif fila == 2 and tableroajedrez[fila-1][columna] != ' ':
+            error = True   # Esta condicion se cumple si arriba hay una ficha
+    else:
+        error = False
+    return error   # Devuelve el boolean asociado a error, para saber si una ficha esta encerrada o no
+
 # Creamos el tablero vacio
 tableroajedrez = [
 [' ', ' ', ' '], 
@@ -39,3 +53,9 @@ tableroajedrez[c][2] = chr(0x265C)
 
 printeartablero(tableroajedrez)   # Printamos el primer tablero, con las piezas en una posicion random
 
+errorx = encerrada(x, 0)
+errory = encerrada(y, 1)
+errorz = encerrada(z, 2)
+errora = encerrada(a, 0)
+errorb = encerrada(b, 1)
+errorc = encerrada(c, 2)
