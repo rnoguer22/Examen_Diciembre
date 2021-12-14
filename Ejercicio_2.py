219a4a24838da8ecd9ee036ac6d934238a543bd7
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-14 11:21:02 +0100

    Importamos la libreria "random" Y añadimos algunas funciones de dicha libreria

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,6 +1,16 @@
+import random
+
 # Creamos el tablero vacio
 tableroajedrez = [
 [' ', ' ', ' '], 
 [' ', ' ', ' '],
 [' ', ' ', ' '], 
 ]
+
+# Creamos variables aleatorias que utilizaremos proximamente
+x = random.randint(0,2)
+y = random.randint(0,2)
+z = random.randint(0,2)
+a = random.randint(0,2)
+b = random.randint(0,2)
+c = random.randint(0,2)
