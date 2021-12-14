52a027b7dee05ce0fab7dafd5af475ae8236c387
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-14 11:03:11 +0100

    Introducimos una funcion para elegir la palabra

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -10,8 +10,17 @@ def minion_game(palabra):
             Puntos_Kevin += 6 - i   # Si la letra es una vocal, calcula los puntos de Kevin
     return [Puntos_Kevin, Puntos_Stuart]
 
+def elegir_palabra():   # Definimos una funcion para elegir la palabra que queramos
+    while True:
+        palabrita = input("Introduzca la palabra: ")
+        if palabrita == str(palabrita):   # De esta manera nos aseguramos que el usuario introduce una palabra y no otra cosa
+            return palabrita
+            break
+        else:
+            print ("Introduzca una palabra, no me trolees por favor")
+
 if __name__ == '__main__':
-    palabra = input("Introduzca la palabra: ")
+    palabra = elegir_palabra()
     vocales = ["a", "e", "i", "o", "u"]   # Definimos las vocales en una lista
     resultado = minion_game(palabra)
     print ("Los puntos de Kevin son {} y los de Stuart son {}".format(resultado[0], resultado[1]))
