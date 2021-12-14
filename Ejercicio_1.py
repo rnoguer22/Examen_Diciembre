9d692343251b446e61692803045670907d267e78
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-14 10:51:18 +0100

    Solucion de errores La funcion nos devuelve una lista con la puntacion de los dos jugadores

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -5,9 +5,10 @@ def minion_game(string):
     # Elaboramos un bucle que nos calculara los puntos de cada jugador
     for i in range(6):
         if palabra[i] not in vocales:   # Si la letra no es una vocal, calcula los puntos de Stuart
-            Puntos_Stuart = 6 - i
+            Puntos_Stuart += 6 - i
         else:
-            Puntos_Kevin = 6 - i   # Si la letra es una vocal, calcula los puntos de Kevin
+            Puntos_Kevin += 6 - i   # Si la letra es una vocal, calcula los puntos de Kevin
+    return [Puntos_Kevin, Puntos_Stuart]
 
 if __name__ == '__main__':
     palabra = "Banana"
