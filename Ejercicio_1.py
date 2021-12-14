68f4d10c9858770f59b248608af8fa2e1917db9f
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-14 10:50:06 +0100

    Bucle que calcula los puntos de Kevin y Stuart

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -2,7 +2,12 @@ def minion_game(string):
     # Establecemos los puntos de ambos jugadores como 0
     Puntos_Kevin = 0
     Puntos_Stuart = 0
-
+    # Elaboramos un bucle que nos calculara los puntos de cada jugador
+    for i in range(6):
+        if palabra[i] not in vocales:   # Si la letra no es una vocal, calcula los puntos de Stuart
+            Puntos_Stuart = 6 - i
+        else:
+            Puntos_Kevin = 6 - i   # Si la letra es una vocal, calcula los puntos de Kevin
 
 if __name__ == '__main__':
     palabra = "Banana"
