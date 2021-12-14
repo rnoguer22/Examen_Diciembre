9c939d7fd1226932ba7c2e434cf74e29e098b9d1
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-14 10:46:40 +0100

    Establecemos los puntos de ambos jugadores como 0

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,5 +1,8 @@
 def minion_game(string):
-    
+    # Establecemos los puntos de ambos jugadores como 0
+    Puntos_Kevin = 0
+    Puntos_Stuart = 0
+
 
 if __name__ == '__main__':
     palabra = "Banana"
